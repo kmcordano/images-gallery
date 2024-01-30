a9cc12ce0d33130823ffaff58996ada4a907ed64
Kevin Cordano <kmcordano@gmail.com>
2024-01-29 20:02:18 -0600

    Polished app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header'
 import Search from './components/Search'
 
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word, setWord] = useState('')
@@ -12,9 +12,7 @@ const App = () => {
     e.preventDefault();
     console.log(word);
   }
-
-  console.log(process.env.REACT_APP_UNSPLASH_KEY);
-  
+ 
   return (
     <div>
       <Header title="Images Gallery"/>
