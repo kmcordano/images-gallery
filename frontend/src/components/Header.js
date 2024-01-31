02d1ac020d1b86dd63c26e629b51d9d25febdb3e
Kevin Cordano <kmcordano@gmail.com>
2024-01-31 16:46:13 -0600

    Welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} data-bs-theme="light">
       <Container>
-        <Logo style={{ maxWidth: "12rem", maxHeight: "4rem" }} />
+        <Logo alt={title} style={{ maxWidth: "12rem", maxHeight: "4rem" }} />
       </Container>
     </Navbar>
   );
