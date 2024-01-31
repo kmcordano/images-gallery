ff6fe6a38deb9ab3df0a7eb245fd80318e9510f0
Kevin Cordano <kmcordano@gmail.com>
2024-01-30 18:34:24 -0600

    Added clear function to search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header'
-import Search from './components/Search'
+import Header from './components/Header';
+import Search from './components/Search';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState('')
+  const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -18,7 +18,8 @@ const App = () => {
       })
       .catch((err) => {
         console.log(err); 
-      })
+      });
+    setWord('');
   }
  
   return (
